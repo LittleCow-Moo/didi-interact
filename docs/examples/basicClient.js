ff8201eb41f33a85c03ef043cca69dc05bc47f79
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-07-27 18:48:14 +0800

    basic client for didi-interact

---
+++ docs/examples/basicClient.js
@@ -0,0 +1,16 @@
+const { DidiClient } = require("didi-interact");
+const { Client } = require("discord.js");
+const client = new Client({
+  intents: [...] // Intents here
+})
+const dc = new DidiClient({
+  client: client,
+  endpoint: "channel-id" // Interaction Channel ID
+})
+
+client.on("ready", ()=>{
+  console.log("ready");
+  dc.send_test(); // REMINDER: send_test should always run when the Discord Bot is ready!
+})
+
+client.login("Z290IHNvbWUgbW9uZXkgZ290IHNvbWUgY2FzaCBpbSByaWNoIG5vdyBpbSBzYWQ=") // It's not a real token, use your token here!
